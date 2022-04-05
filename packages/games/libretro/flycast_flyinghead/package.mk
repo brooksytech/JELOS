e456941d84acd05f8386f250c31a55962a9496a3
Brooksytech <macebrooks@gmail.com>
2022-04-05 05:57:24 -0700

    Update package.mk

--- packages/games/libretro/flycast_flyinghead/package.mk
+++ packages/games/libretro/flycast_flyinghead/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
 
-PKG_NAME="flycast_flyinghead"
+PKG_NAME="flycast"
 PKG_VERSION="e46d819c465ef342f5e9ef4c683231b17f56f206"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="$PKG_SITE.git"
@@ -20,5 +20,5 @@ pre_configure_target() {
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp flycast_libretro.so $INSTALL/usr/lib/libretro/flycast_libretro.so
+  cp flycast_libretro.so $INSTALL/usr/lib/libretro/*
 }
