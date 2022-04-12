8ba8e26158b5923d731cbf342a4d1d1d83b272f4
Brooksytech <macebrooks@gmail.com>
2022-04-12 14:21:03 -0700

    Update package.mk

--- packages/games/emulators/flycastsa/package.mk
+++ packages/games/emulators/flycastsa/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="flycastsa"
-PKG_VERSION="3d5fd61285a2b57ed4ac7b2b25bc2b830cf43d57"
+PKG_VERSION="fd50529d864c0af7007f2f283a84bf67851fe0e4"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="$PKG_SITE.git"
