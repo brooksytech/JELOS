ffe19c9ff331faa5ee62ae6a7cf73baae267ffdd
Brooksytech <macebrooks@gmail.com>
2022-04-15 11:05:59 -0700

    Adding MelonDS

---
+++ packages/games/libretro/melonds/package.mk
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present BrooksyTech (https://github.com/brooksytech)
+
+PKG_NAME="melonds"
+PKG_VERSION="e93ec3e462d3dfc1556781510a3cee113f02abb2"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://git.libretro.com/libretro/melonDS"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain nasm:host $OPENGLES"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="MeloDS - Nintendo DS emulator for libretro"
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="-lto"
+PKG_PATCH_DIRS+="${DEVICE}"
+
+pre_configure_target() {
+  if [[ "${DEVICE}" =~ RG351 ]]
+  then
+    PKG_MAKE_OPTS_TARGET=" platform=odroidgoa"
+  elif [[ "${DEVICE}" =~ RG552 ]]
+  then
+    PKG_MAKE_OPTS_TARGET=" platform=RK3399"
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp melonds_libretro.so $INSTALL/usr/lib/libretro/
+}
