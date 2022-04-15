80109183b3cbea1b4b9fe425e97db580895ca52f
Brooksytech <macebrooks@gmail.com>
2022-04-15 15:37:53 -0700

    Update package.mk

--- packages/games/libretro/melonds/package.mk
+++ packages/games/libretro/melonds/package.mk
@@ -1,21 +1,18 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2022-present BrooksyTech (https://github.com/brooksytech)
-
-PKG_NAME="melonds"
-PKG_VERSION="6abea20344d5fcd9e953b9bb994625122262f111"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/brooksytech/melonds-libretro"
-PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://git.libretro.com/libretro/melonDS"
+PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_TARGET="toolchain $OPENGLES"
 PKG_PRIORITY="optional"
 PKG_SECTION="libretro"
 PKG_SHORTDESC="MeloDS - Nintendo DS emulator for libretro"
 PKG_TOOLCHAIN="make"
 
+rm -rf CmakeList.txt
+
 pre_make_target() {
-  
+
   if [[ "${DEVICE}" =~ RG351 ]]
   then
     PKG_MAKE_OPTS_TARGET=" platform=odroidgoa"
