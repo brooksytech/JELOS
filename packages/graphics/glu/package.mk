aedcf33e9b9937faec01ee0fb3a0bd406e08d2db
brooksytech <macebrooks@gmail.com>
2022-08-09 08:43:50 -0700

    RG552 5.10

--- packages/graphics/glu/package.mk
+++ packages/graphics/glu/package.mk
@@ -9,7 +9,6 @@ PKG_LICENSE="OSS"
 PKG_SITE="https://gitlab.freedesktop.org/mesa/glu/"
 PKG_URL="https://mesa.freedesktop.org/archive/glu/glu-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libglvnd mesa"
-PKG_DEPENDS_TARGET="toolchain libglvnd mesa"
 PKG_NEED_UNPACK="$(get_pkg_directory mesa)"
 PKG_LONGDESC="libglu is the The OpenGL utility library"
 
